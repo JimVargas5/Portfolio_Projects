0eb133e76f6032ca1e1558a35562bac59b5dc8d8
Jim V <jimball1@hotmail.com>
2024-01-29 20:22:25 -0800

    prettied up some SQL queries

--- CoronaVirus/Covid_portfolio_queries/Covid_2.sql
+++ CoronaVirus/Covid_portfolio_queries/Covid_2.sql
@@ -48,6 +48,36 @@ to the different approaches.
 
 
 
+--Same as above but with temp tables
+DROP TABLE IF EXISTS #PercentPopVacc
+CREATE TABLE #PercentPopVacc (
+	continent nvarchar(255),
+	location nvarchar(255),
+	date datetime,
+	population numeric,
+	new_vaccinations numeric,
+	total_vacc numeric
+)
+
+INSERT INTO #PercentPopVacc
+SELECT deaths.continent, deaths.location, deaths.date, deaths.population,
+	vacc.new_vaccinations,
+	SUM(CONVERT(bigint, vacc.new_vaccinations)) OVER
+		(PARTITION BY deaths.location ORDER BY deaths.location, deaths.date)
+		AS 'Total Vacc. (by location and date)'
+From Portfolio_Project_Covid..CovidDeaths AS deaths
+JOIN Portfolio_Project_Covid..CovidVaccinations AS vacc
+	ON deaths.location = vacc.location
+	AND deaths.date = vacc.date
+--WHERE deaths.continent IS NOT NULL
+
+SELECT *, (total_vacc/population)*100 AS 'Running Vacc. Rate (%)'
+FROM #PercentPopVacc
+WHERE location = 'Israel'
+
+
+
+
 --Finding countries with highest vacc. rate
 WITH CTE_VaccMAX (continent, location, population, [Total Vacc. (by location and date)])
 AS
@@ -75,32 +105,7 @@ Double vax?
 
 
 
---Same as above but with temp tables
-DROP TABLE IF EXISTS #PercentPopVacc
-CREATE TABLE #PercentPopVacc (
-	continent nvarchar(255),
-	location nvarchar(255),
-	date datetime,
-	population numeric,
-	new_vaccinations numeric,
-	total_vacc numeric
-)
 
-INSERT INTO #PercentPopVacc
-SELECT deaths.continent, deaths.location, deaths.date, deaths.population,
-	vacc.new_vaccinations,
-	SUM(CONVERT(bigint, vacc.new_vaccinations)) OVER
-		(PARTITION BY deaths.location ORDER BY deaths.location, deaths.date)
-		AS 'Total Vacc. (by location and date)'
-From Portfolio_Project_Covid..CovidDeaths AS deaths
-JOIN Portfolio_Project_Covid..CovidVaccinations AS vacc
-	ON deaths.location = vacc.location
-	AND deaths.date = vacc.date
---WHERE deaths.continent IS NOT NULL
-
-SELECT *, (total_vacc/population)*100 AS 'Running Vacc. Rate (%)'
-FROM #PercentPopVacc
-WHERE location = 'Israel'
 
 
 
--- CoronaVirus/Covid_portfolio_queries/Covid_3.sql
+++ CoronaVirus/Covid_portfolio_queries/Covid_3.sql
@@ -22,15 +22,6 @@ WHERE continent IS NULL
 GROUP BY location
 ORDER BY [Total Death Count] DESC
 
-/*
-The data tallies up statistics for continents in various rows (as well as EU and the whole world, etc.),
-so we filter "WHERE continent is NULL" to find the "Europe" tallied row, etc.
-
-In calculating 'Death Rate', SQL throws an error when applying the operations to scale by 100
-and divide by 'population' *after* the SUM operation, but not *before*.
-Applying these operations *before* (should?) increase the number of operations for this querie.
-*/
-
 
 
 
@@ -40,10 +31,6 @@ SELECT location, population, MAX(total_cases) AS 'Max Infection Count',
 FROM Portfolio_Project_Covid..CovidDeaths
 GROUP BY location, population
 ORDER BY 4 DESC
-
-/*
-Format NULLs in this table to 0 (zeros)
-*/
 
 
 
